2797eb589c5013b69e7d263fefd50c61716d9318
rjs1197 <gmj1197@gmail.com>
2018-06-20 18:21:34 +0900

    feat: Add numpyNplotTest.py
    Add numpy library and matplotlib library. also, add .gitignore for jetBrains

---
+++ machine_learning/acting/numpyNplotTest.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin & cos')
+plt.legend()
+plt.show()
+
